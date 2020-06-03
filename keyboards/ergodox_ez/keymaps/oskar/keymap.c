f42f8d8ad386a6f4cf2c2fb310b718fe74efc12c
Oskar Holstensson <oskar@holstensson.se>
2020-06-03 08:46:16 +0200

    Moved ctrl, shift from the left pinky finger

--- keyboards/ergodox_ez/keymaps/oskar/keymap.c
+++ keyboards/ergodox_ez/keymaps/oskar/keymap.c
@@ -26,48 +26,46 @@ enum custom_keycodes {
   USR_CAPS,
 };
 
-#define ___ KC_TRANSPARENT
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
-    ___,        BP_BSLS,  BP_ECUT,  BP_DCRC,  BP_SLSH,  BP_UNDS,     ___,         ___,   BP_DTRM,     BP_EGRV,  BP_MINS,  BP_CCED,     BP_AGRV,        BP_UGRV,
-    KC_ESCAPE,  BP_Q,     BP_W,     BP_E,     BP_R,     BP_T,        KC_INSERT,  TO(2),  BP_Y,        BP_U,     BP_I,     BP_O,        BP_P,           USR_ARING,
-    KC_RCTRL,   BP_A,     BP_S,     BP_D,     BP_F,     BP_G,                            BP_H,        BP_J,     BP_K,     BP_L,        USR_ODIAE,      USR_ADIAE,
-    KC_LSHIFT,  BP_Z,     BP_X,     BP_C,     BP_V,     LT(2,BP_B),  KC_DELETE,  TO(3),  LT(3,BP_N),  BP_M,     BP_COMM,  BP_DOT,      BP_SCLN,        KC_RSHIFT,
-    USR_CAPS,   ___,      ___,      KC_LGUI,  KC_LALT,                                                KC_RALT,  KC_RGUI,  KC_PSCREEN,  KC_SCROLLLOCK,  KC_PAUSE,
-                                                  LALT_T(KC_APPLICATION),  ___,  ___,  KC_RCTRL,
-                                                                       KC_HOME,  KC_PGUP,
-                                           LT(1,KC_SPACE),  KC_BSPACE,  KC_END,  KC_PGDOWN,  KC_TAB,  LT(1,KC_ENTER)
+    KC_PSCREEN,  BP_BSLS,  BP_ECUT,  BP_DCRC,   BP_SLSH,  BP_UNDS,  KC_INSERT,  KC_SCROLLLOCK,  BP_DTRM,  BP_EGRV,    BP_MINS,   BP_CCED,         BP_AGRV,        BP_UGRV,
+    KC_PAUSE,    BP_Q,     BP_W,     BP_E,      BP_R,     BP_T,     KC_DELETE,  KC_TAB,         BP_Y,     BP_U,       BP_I,      BP_O,            BP_P,           USR_ARING,
+    KC_ESCAPE,   BP_A,     BP_S,     BP_D,      BP_F,     BP_G,                                 BP_H,     BP_J,       BP_K,      BP_L,            USR_ODIAE,      USR_ADIAE,
+    USR_CAPS,    BP_Z,     BP_X,     BP_C,      BP_V,     BP_B,     MO(2),      MO(3),          BP_N,     BP_M,       BP_COMM,   BP_DOT,          BP_SCLN,        BP_SQUOT,
+    _______,     _______,  _______,  KC_RCTRL,  KC_LSHIFT,                                                KC_RSHIFT,  KC_RCTRL,  KC_APPLICATION,  _______,        _______,
+                                                            KC_LALT,  KC_LGUI,  KC_RGUI,  KC_LALT,
+                                                                      KC_HOME,  KC_PGUP,
+                                                KC_SPACE,  KC_BSPACE,  KC_END,  KC_PGDOWN,  MO(1),  KC_ENTER
   ),
   [1] = LAYOUT_ergodox_pretty(
-    ___,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    ___,  ___,  KC_F6,    KC_F7,    KC_F8,     KC_F9,    KC_F10,   KC_F11,
-    ___,  BP_EXLM,  BP_AT,    BP_HASH,  BP_DLR,   BP_PERC,  ___,  ___,  BP_CIRC,  BP_AMPR,  BP_TILD,   BP_EQL,   BP_ASTR,  KC_F12,
-    ___,  BP_LCBR,  BP_RCBR,  BP_LPRN,  BP_RPRN,  BP_EURO,              BP_PIPE,  BP_LBRC,  BP_RBRC,   BP_LESS,  BP_GRTR,  BP_GRV,
-    ___,  BP_MDSH,  BP_NDSH,  BP_LGIL,  BP_RGIL,  BP_OE,    ___,  ___,  BP_AE,    BP_DQOT,  BP_SQUOT,  BP_QEST,  BP_COLN,  ___,
-    ___,  ___,      ___,      ___,      ___,                                      ___,      ___,       ___,      ___,      ___,
-                                                      ___,  ___,  ___,  ___,
-                                                            ___,  ___,
-                                                ___,  ___,  ___,  ___,  ___,  ___
+    _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______,  _______,  KC_F6,    KC_F7,    KC_F8,     KC_F9,    KC_F10,   KC_F11,
+    _______,  BP_EXLM,  BP_AT,    BP_HASH,  BP_DLR,   BP_PERC,  _______,  _______,  BP_CIRC,  BP_AMPR,  BP_TILD,   BP_EQL,   BP_ASTR,  KC_F12,
+    _______,  BP_LCBR,  BP_RCBR,  BP_LPRN,  BP_RPRN,  BP_EURO,                      BP_PIPE,  BP_LBRC,  BP_RBRC,   BP_LESS,  BP_GRTR,  _______,
+    _______,  BP_MDSH,  BP_NDSH,  BP_LGIL,  BP_RGIL,  BP_OE,    _______,  _______,  BP_AE,    BP_DQOT,  BP_GRV,    BP_QEST,  BP_COLN,  _______,
+    _______,  _______,  _______,  _______,  _______,                                           _______,      _______,       _______,      _______,      _______,
+                                                      _______,  _______,  _______,  _______,
+                                                                _______,  _______,
+                                            _______,  _______,  _______,  _______,  _______,  _______
   ),
   [2] = LAYOUT_ergodox_pretty(
-    ___,  ___,  ___,  ___,  ___,  ___,  ___,  ___,    ___,  ___,   BP_SLSH,  BP_ASTR,  ___,          ___,
-    ___,  ___,  ___,  ___,  ___,  ___,  ___,  TO(0),  ___,  BP_7,  BP_8,     BP_9,     BP_MINS,      ___,
-    ___,  ___,  ___,  ___,  ___,  ___,                ___,  BP_4,  BP_5,     BP_6,     BP_PLUS,      ___,
-    ___,  ___,  ___,  ___,  ___,  ___,  ___,  ___,    ___,  BP_1,  BP_2,     BP_3,     KC_KP_ENTER,  ___,
-    ___,  ___,  ___,  ___,  ___,                            BP_0,  ___,      BP_DOT,   ___,          ___,
-                                  ___,  ___,  ___,  ___,
-                                        ___,  ___,
-                            ___,  ___,  ___,  ___,  ___,  ___
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   BP_SLSH,  BP_ASTR,  _______,      _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  BP_7,      BP_8,     BP_9,     BP_MINS,      _______,
+    _______,  _______,  _______,  _______,  _______,  _______,                      _______,  BP_4,      BP_5,     BP_6,     BP_PLUS,      _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  BP_1,      BP_2,     BP_3,     KC_KP_ENTER,  _______,
+    _______,  _______,  _______,  _______,  _______,                                          BP_0,      _______,  BP_DOT,   _______,      _______,
+                                                      _______,  _______,  _______,  _______,
+                                                                _______,  _______,
+                                            _______,  _______,  _______,  _______,  _______,  _______
   ),
   [3] = LAYOUT_ergodox_pretty(
-    ___,  ___,            ___,            ___,          ___,             ___,                ___,  ___,    ___,                  ___,        ___,       ___,      ___,       RESET,
-    ___,  KC_MS_WH_LEFT,  KC_MS_WH_DOWN,  KC_MS_WH_UP,  KC_MS_WH_RIGHT,  KC_AUDIO_VOL_UP,    ___,  ___,    KC_BRIGHTNESS_UP,    LED_LEVEL,  KC_PGDOWN,  KC_PGUP,  ___,       ___,
-    ___,  KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_UP,     KC_MS_RIGHT,     KC_AUDIO_VOL_DOWN,                KC_BRIGHTNESS_DOWN,  KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT,  ___,
-    ___,  ___,            ___,            ___,          ___,             KC_AUDIO_MUTE,      ___,  TO(0),  ___,                 ___,        ___,        ___,      ___,       ___,
-    ___,  ___,            ___,            KC_WWW_BACK,  KC_WWW_FORWARD,                                                         ___,        ___,        ___,      ___,       ___,
-                                                                                ___,  KC_MS_BTN5,  ___,  ___,
-                                                                                      KC_MS_BTN4,  ___,
-                                                            KC_MS_BTN1,  KC_MS_BTN2,  KC_MS_BTN3,  ___,  ___,  ___
+    _______,  _______,        _______,        _______,      _______,         _______,            _______,  _______,  _______,             _______,    _______,    _______,  _______,   RESET,
+    _______,  KC_MS_WH_LEFT,  KC_MS_WH_DOWN,  KC_MS_WH_UP,  KC_MS_WH_RIGHT,  KC_AUDIO_VOL_UP,    _______,  _______,  KC_BRIGHTNESS_UP,    LED_LEVEL,  KC_PGDOWN,  KC_PGUP,  _______,   _______,
+    _______,  KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_UP,     KC_MS_RIGHT,     KC_AUDIO_VOL_DOWN,                      KC_BRIGHTNESS_DOWN,  KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT,  _______,
+    _______,  _______,        _______,        KC_WWW_BACK,  KC_WWW_FORWARD,  KC_AUDIO_MUTE,      _______,  _______,  _______,             _______,    _______,    _______,  _______,   _______,
+    _______,  _______,        _______,        _______,      _______,                                                                      _______,    _______,    _______,  _______,   _______,
+                                                                                       _______,  _______,  _______,  _______,
+                                                                                                 _______,  _______,
+                                                          KC_MS_BTN1,  KC_MS_BTN2,  KC_MS_BTN3,  _______,  _______,  _______
   ),
 };
 
